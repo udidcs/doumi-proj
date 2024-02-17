278ec9ba4b2cad7b31f78ce56ed292e0e40ce8a5
fernandokkang <spineattack@gmail.com>
2024-02-18 06:46:31 +0900

    test: Add test code for searching posts by author and title

--- src/main/java/com/example/doumiproject/repository/PostRepository.java
+++ src/main/java/com/example/doumiproject/repository/PostRepository.java
@@ -12,6 +12,7 @@ public interface PostRepository {
     public List<PostDto> findAllQuiz(int page, int pageSize);
     public List<PostDto> findAllQuiz();
     public int getTotalPages(int pageSize);
+    public List<PostDto> findByTitleOrAuthor(String keyword, int page, int pageSize);
 
     default RowMapper<PostDto> postDtoRowMapper() {
         return ((rs, rowNum) -> {
--- src/test/java/com/example/doumiproject/repository/PostRepositoryTest.java
+++ src/test/java/com/example/doumiproject/repository/PostRepositoryTest.java
@@ -1,14 +1,13 @@
 package com.example.doumiproject.repository;
 
 import com.example.doumiproject.dto.PostDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PostRepositoryTest {
@@ -31,7 +30,7 @@ class PostRepositoryTest {
     @DisplayName("모든 퀴즈 목록 페이징 적용해서 출력하기")
     public void findAllQuizWithPaginationTest() {
 
-        List<PostDto> quizs = postRepository.findAllQuiz(2, 8);
+        List<PostDto> quizs = postRepository.findAllQuiz(2, 10);
 
         for(PostDto postDto : quizs) {
             System.out.println(postDto.getId()+" "+postDto.getTitle()+" "+postDto.getContents());
@@ -42,6 +41,19 @@ class PostRepositoryTest {
     @DisplayName("총 페이지 수 출력하기")
     public void getTotalPagesTest() {
 
-        System.out.println(postRepository.getTotalPages(10));
+        Assertions.assertEquals(8, postRepository.getTotalPages(10));
+    }
+
+    @Test
+    @DisplayName("제목 or 작성자 검색 결과 출력하기")
+    public void findByTitleOrAuthorTest() {
+
+        String search = "퀴즈";
+
+        List<PostDto> quizs = postRepository.findByTitleOrAuthor(search, 3, 10);
+
+        for(PostDto postDto : quizs) {
+            System.out.println(postDto.getId()+" "+postDto.getTitle()+" "+postDto.getContents());
+        }
     }
 }
